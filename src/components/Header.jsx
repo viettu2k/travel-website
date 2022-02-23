168b494de77cd1f07aca9266e3d643e399e1e51b
viettu2k <doanviettu2k@gmail.com>
2022-02-23 16:33:46 +0700

    video poster attribute

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,12 +4,19 @@ import { useState } from "react";
 export default function Header() {
   const [state] = useState({
     video: "/assets/videos/header.mp4",
+    poster: "/assets/images/screen.png",
   });
 
   return (
     <div className="header">
       <div className="header__video">
-        <video src={state.video} autoPlay loop muted></video>
+        <video
+          src={state.video}
+          autoPlay
+          loop
+          muted
+          poster={state.poster}
+        ></video>
       </div>
     </div>
   );
