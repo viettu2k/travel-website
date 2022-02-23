36d76d5f121118bcbb4212be58a73990751dbf9f
viettu2k <doanviettu2k@gmail.com>
2022-02-23 16:54:39 +0700

    add contents in the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,6 +18,24 @@ export default function Header() {
           poster={state.poster}
         ></video>
       </div>
+      <div className="header__contents">
+        <div className="container">
+          <div className="header__contents__text">
+            <div className="header__contents__text__child">
+              <h1 className="header__contents_text_child_h1">
+                We are travel friends
+              </h1>
+              <p className="header__contents__text__child__p">
+                Come and join us we travel the most famous and beautiful places
+                in the worlds
+              </p>
+              <div className="header__contents__text__child__link">
+                <button>Get Started</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
