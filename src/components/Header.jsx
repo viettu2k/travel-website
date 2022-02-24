3c2a3eed80348b610b2221620f38b60a05af1c5c
viettu2k <doanviettu2k@gmail.com>
2022-02-24 15:55:58 +0700

    add logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,10 +5,16 @@ export default function Header({ heading, paragraph, children }) {
   const [state] = useState({
     video: "/assets/videos/header.mp4",
     poster: "/assets/images/screen.png",
+    logo: "/assets/images/logo.png",
   });
 
   return (
     <div className="header">
+      <div className="container pr">
+        <div className="header__logo">
+          <img src={state.logo} alt="logo" />
+        </div>
+      </div>
       <div className="header__video">
         <video
           src={state.video}
