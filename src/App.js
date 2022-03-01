0855e3a29d7503053c061fb317aac8ea07d1b0e9
viettu2k <doanviettu2k@gmail.com>
2022-03-01 09:58:53 +0700

    create global state

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ModelContext from "./context/ModelContext";
+import Model from "./components/Model";
 
 console.log(ModelContext);
 function App() {
+  const [state, dispatch] = useState({ modelStatus: false });
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/about" component={About} />
-        <Route component={NotFound} />
+        <ModelContext.Provider value={{state, dispatch}} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
+          <Route component={NotFound} />
+        </ModelContext.Provider>
       </Switch>
     </Router>
   );
