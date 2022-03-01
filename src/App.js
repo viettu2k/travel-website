bd75f3a61ca895922484ed4f411a771ebdbc529e
viettu2k <doanviettu2k@gmail.com>
2022-03-01 09:54:31 +0700

    create context oject

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import ModelContext from "./context/ModelContext";
 
+console.log(ModelContext);
 function App() {
   return (
     <Router>
