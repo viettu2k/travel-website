d80f89e4301b9c1750db8379d16483442a343a69
viettu2k <doanviettu2k@gmail.com>
2022-03-08 21:14:02 +0700

    create navigation & refactoring

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Nav = () => {
   return (
     <>
       <div className="navLayer"></div>
+      <div className="nav">
+        <div className="nav__content">
+          <li to="/">
+            <Link>Home</Link>
+          </li>
+          <li to="/about">
+            <Link>About</Link>
+          </li>
+        </div>
+      </div>
     </>
   );
 };
