9fa6d6241e8c04a62225e2ff9192eb648c0716a6
viettu2k <doanviettu2k@gmail.com>
2022-03-08 20:54:36 +0700

    create transparent layer

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ModelProvider from "./context/providers/ModelProvider";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <ModelProvider>
+        <Nav />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
---
+++ src/components/Nav.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Nav = () => {
+  return (
+    <>
+      <div className="navLayer"></div>
+    </>
+  );
+};
+
+export default Nav;
