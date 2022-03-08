c2a9199874069314d2fb89dcafe7864c02c271bb
viettu2k <doanviettu2k@gmail.com>
2022-03-08 21:40:11 +0700

    create nav context & provider

--- src/App.js
+++ src/App.js
@@ -3,18 +3,21 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ModelProvider from "./context/providers/ModelProvider";
+import NavProvider from "./context/providers/NavProvider";
 import Nav from "./components/Nav";
 
 function App() {
   return (
     <Router>
       <ModelProvider>
-        <Nav />
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/about" component={About} />
-          <Route component={NotFound} />
-        </Switch>
+        <NavProvider>
+          <Nav />
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+            <Route component={NotFound} />
+          </Switch>
+        </NavProvider>
       </ModelProvider>
     </Router>
   );
