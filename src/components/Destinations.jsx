a9bf866c0b36c6d82c1a11851038c192f2f2661d
viettu2k <doanviettu2k@gmail.com>
2022-03-15 09:42:23 +0700

    style Destination heading

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -12,10 +12,10 @@ function Destinations() {
     <div className="destinations">
       <div className="container">
         <div className="row">
-          <div className="col-6">
+          <div className="col-6 p-15">
             <h3 className="destinations__heading">{state.heading}</h3>
           </div>
-          <div className="col-6">
+          <div className="col-6 p-15">
             <p className="destinations__paragraph">{state.paragraph}</p>
           </div>
         </div>
