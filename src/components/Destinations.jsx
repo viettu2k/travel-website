a122e49809a5d2093aea5db302847ad07a451910
viettu2k <doanviettu2k@gmail.com>
2022-03-17 14:43:19 +0700

    show contry name on card

--- src/components/Destinations.jsx
+++ src/components/Destinations.jsx
@@ -1,35 +1,36 @@
-import React, { useState, useContext } from "react";
+import { useState, useContext } from "react";
 import DestinationsContext from "../context/DestinationsContext";
 import DestinationsList from "./DestinationsList";
-
-function Destinations() {
+const Destinations = () => {
   const {
     destinationsData: { destinations },
   } = useContext(DestinationsContext);
+
   const [state] = useState({
     heading:
       "Discover the most engaging places in the world with Travel Friends",
     paragraph:
       "Travel is a brutality. It forces you to trust strangers and to lose sight of all that familiar comforts of home and friends.You are constantly of balance. Nothing is yours except the essential things Air, Sleep, Dream, the sea, the sky-all things tending towards the eternal or what we imagine of it.",
   });
-
   return (
     <div className="destinations">
       <div className="container">
         <div className="row ml-minus-15 mr-minus-15">
           <div className="col-6 p-15">
-            <h3 className="destinations__heading">{state.heading}</h3>
+            <h3 className="destinations__heading animation">{state.heading}</h3>
           </div>
           <div className="col-6 p-15">
-            <p className="destinations__paragraph">{state.paragraph}</p>
+            <p className="destinations__paragraph animation">
+              {state.paragraph}
+            </p>
           </div>
         </div>
         <div className="destinations__block">
           <div className="row ml-minus-15 mr-minus-15">
             {destinations.map((destination) => (
               <DestinationsList
-                key={destination.id}
                 destination={destination}
+                key={destination.id}
               />
             ))}
           </div>
@@ -37,6 +38,5 @@ function Destinations() {
       </div>
     </div>
   );
-}
-
+};
 export default Destinations;
--- src/components/DestinationsList.jsx
+++ src/components/DestinationsList.jsx
@@ -1,16 +1,23 @@
-import React from "react";
 import { LazyLoadImage } from "react-lazy-load-image-component";
+import { GiPositionMarker } from "react-icons/gi";
 
 const DestinationsList = ({ destination }) => {
   return (
     <div className="col-3 p-15">
-      <div className="destinations__card">
-        <div className="destination__card__img">
+      <div className="destinations__card animation">
+        <div className="destinations__card__img">
           <LazyLoadImage src={destination.image} alt={destination.image} />
+        </div>
+        <div className="destinations__card__layer">
+          <div className="destinations__card__layer__content">
+            <GiPositionMarker size={20} />
+            <div className="destinations__card__layer__content__country">
+              {destination.name}
+            </div>
+          </div>
         </div>
       </div>
     </div>
   );
 };
-
 export default DestinationsList;
