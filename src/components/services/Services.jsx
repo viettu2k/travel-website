4107a739559dd073e76d0914eb4099deb4bc24b3
viettu2k <doanviettu2k@gmail.com>
2022-03-27 10:31:52 +0700

    services subheading

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import ServicesLeft from "./ServicesLeft";
 
 const Services = () => {
+  const [state] = useState({
+    heading:
+      "Why customers love travel friends? because we provide unique services.",
+    subHeading:
+      "our aim is to provide professional and unique services to customers, and we have provided high quality services to our customers that why they love travel friends.",
+  });
   return (
     <div className="services">
       <div className="services__contents">
         <div className="container">
           <div className="row ml-minus-15 mr-minus-15">
             <div className="col-6 p-15">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-              commodi quis beatae perferendis accusantium sit ullam animi
-              adipisci dicta a, reprehenderit laudantium aliquid voluptatibus,
-              deserunt quibusdam distinctio eligendi eius itaque?
+              <ServicesLeft
+                heading={state.heading}
+                subHeading={state.subHeading}
+              />
             </div>
             <div className="col-6 p-15">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
