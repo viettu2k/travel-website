15f565cc713270a484856bdad283b996a7fe6f12
viettu2k <doanviettu2k@gmail.com>
2022-03-28 17:09:26 +0700

    show services details

--- src/components/services/ServicesList.jsx
+++ src/components/services/ServicesList.jsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiChevronRight } from "react-icons/bi";
 
 const ServicesList = ({ services }) => {
+  const [current, setCurrent] = useState(0);
+  const showDetails = (index) => {
+    setCurrent(index);
+  };
+
   return services.length > 0
     ? services.map((service, index) => (
         <div className="services__list" key={index}>
-          <h4 className="services__list__heading">
+          <h4
+            className="services__list__heading"
+            onClick={() => showDetails(index)}
+          >
             <BiChevronRight size={25} />
             {service.heading}
           </h4>
-          <p className="services__list__details">{service.p}</p>
+          <p
+            className={`services__list__details ${
+              current === index ? "services__list__details--show" : ""
+            }`}
+          >
+            {service.p}
+          </p>
         </div>
       ))
     : "";
