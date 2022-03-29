ce58d9ce6c41cb2389fd4204c579ed12fb15c43a
viettu2k <doanviettu2k@gmail.com>
2022-03-29 16:44:03 +0700

    rating function

--- src/components/reviews/ReviewBody.jsx
+++ src/components/reviews/ReviewBody.jsx
@@ -1,6 +1,31 @@
 import React from "react";
+import { BsFillStarFill, BsStar } from "react-icons/bs";
 
 const ReviewBody = ({ review }) => {
+  const rating = (number) => {
+    let container = [];
+    for (let i = 1; i <= 5; i++) {
+      if (i <= number) {
+        container.push(
+          <BsFillStarFill
+            size={13}
+            color="#df2189"
+            className="reviews__body__contents__info__rating__icon"
+          />
+        );
+      } else {
+        container.push(
+          <BsStar
+            size={13}
+            color="#df2189"
+            className="reviews__body__contents__info__rating__icon"
+          />
+        );
+      }
+    }
+    return container;
+  };
+
   return (
     <div className="col-4 p-15">
       <div className="reviews__body">
@@ -13,7 +38,7 @@ const ReviewBody = ({ review }) => {
               {review.name}
             </div>
             <div className="reviews__body__contents__info__rating">
-              {review.starts}
+              {rating(review.stars)}
             </div>
             <div className="reviews__body__contents__info__comment">
               {review.comment}
