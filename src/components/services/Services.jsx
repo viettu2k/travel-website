7ea4c57874213f96de36daadb2ce0c6a979e7442
viettu2k <doanviettu2k@gmail.com>
2022-03-29 11:24:49 +0700

    align services contents vertically

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -18,7 +18,7 @@ const Services = () => {
     <div className="services">
       <div className="services__contents">
         <div className="container">
-          <div className="row ml-minus-15 mr-minus-15">
+          <div className="row ml-minus-15 mr-minus-15 services__contents__container">
             <div className="col-6 p-15">
               <ServicesLeft
                 heading={state.heading}
--- src/components/services/ServicesLeft.jsx
+++ src/components/services/ServicesLeft.jsx
@@ -4,7 +4,7 @@ const ServicesLeft = ({ heading, subHeading }) => {
   return (
     <div className="services__left">
       <h1 className="services__left__heading">{heading}</h1>
-      <p className="services__left_paragraph">{subHeading}</p>
+      <p className="services__left__paragraph">{subHeading}</p>
     </div>
   );
 };
