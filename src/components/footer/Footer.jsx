68b9588603db274106a920d8ab294ca76d5f3c1a
viettu2k <doanviettu2k@gmail.com>
2022-03-31 10:19:32 +0700

    show logo & footer intro

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import FooterIntro from "./FooterIntro";
 
 const Footer = () => {
   const footerStyles = {
@@ -14,7 +15,9 @@ const Footer = () => {
         <div className="container">
           <div className="footer__container__block">
             <div className="row ml-minus-15 mr-minus-15">
-              <div className="col-6 p-15"></div>
+              <div className="col-6 p-15">
+                <FooterIntro />
+              </div>
               <div className="col-6 p-15"></div>
             </div>
           </div>
---
+++ src/components/footer/FooterIntro.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+
+const FooterIntro = () => {
+  const [state] = useState({
+    logo: "/assets/footer/logo.png",
+    intro:
+      "Travel friends is a traveling agency, we arrange tours to the world famous and most beautiful countries, Since 2005 we provide hight quality services to our valuable customers.",
+  });
+
+  return (
+    <div className="footer__intro">
+      <div className="footer__intro__img">
+        <LazyLoadImage src={state.logo} alt="footer logo" />
+      </div>
+      <div className="footer__intro__msg">{state.intro}</div>
+    </div>
+  );
+};
+
+export default FooterIntro;
