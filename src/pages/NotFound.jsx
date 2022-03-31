831cd26da85ef3ce3974cd25ebd35b3cf007cd1a
viettu2k <doanviettu2k@gmail.com>
2022-03-31 23:03:39 +0700

    style not found page

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
 
 export default function NotFound() {
+  const [state] = useState({
+    heading: "Oops, 404",
+    paragraph: "Sorry, the page you are look for does not exist.",
+    image: "/assets/images/404.jpg",
+  });
+
   return (
     <>
       <Helmet>
@@ -9,6 +17,15 @@ export default function NotFound() {
         <meta name="description" content="travel friends not found page" />
       </Helmet>
       <h1>NotFound</h1>;
+      <Header
+        heading={state.heading}
+        paragraph={state.paragraph}
+        image={state.image}
+      >
+        <Link className="btn-default" to="/">
+          go to home
+        </Link>
+      </Header>
     </>
   );
 }
