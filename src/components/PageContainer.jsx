7a008fd74c7cea4e4a884271eb33667a8e9c581e
viettu2k <doanviettu2k@gmail.com>
2022-03-31 20:43:23 +0700

    create page container

---
+++ src/components/PageContainer.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Header from "./Header";
+import { Link } from "react-router-dom";
+
+const PageContainer = ({ data }) => {
+  return (
+    <>
+      <Header heading={data.heading} />
+      <div className="page">
+        <div className="container">
+          <div className="row">
+            <div className="col-6"></div>
+            <div className="col-6">
+              <div className="page__info">
+                <h2 className="heading">{data.heading}</h2>
+                <h1 className="page__info__heading">{data.pageHeading}</h1>
+                <p className="page__info__msg">{data.msg}</p>
+                <div className="page__info__link">
+                  <Link to="/" className="btn-dark">
+                    go back
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PageContainer;
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { Helmet } from "react-helmet-async";
+import PageContainer from "../components/PageContainer";
+
 export default function About() {
+  const [state] = useState({
+    heading: "about us",
+    pageHeading: "world best travel agency company since 2005",
+    msg: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore porro vel consequuntur minus id quam in vitae saepe veniam. Alias, tempore distinctio laborum molestiae odio dicta corrupti quia debitis! Deserunt?",
+  });
+
   return (
     <>
       <Helmet>
         <title>About page</title>
         <meta name="description" content="travel friends about page" />
       </Helmet>
-      <h1>About page</h1>
+      <PageContainer data={state} />
     </>
   );
 }
