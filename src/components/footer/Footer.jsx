37dd1fc6310ed94b4c167da7a272ff1776b77ce5
viettu2k <doanviettu2k@gmail.com>
2022-03-31 10:11:26 +0700

    footer background image

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 
 const Footer = () => {
+  const footerStyles = {
+    background: "url(assets/footer/footer.jpg)",
+    backgroundAttachment: "fixed",
+    backgroundSize: "cover",
+    backgroundRepeat: "no-repeat",
+  };
+
   return (
-    <footer className="footer">
+    <footer className="footer" style={footerStyles}>
       <div className="footer__container">
         <div className="container">
           <div className="footer__container__block">
